a93701d34c63eb7fe53053da9049ab08f69bd79c
AntonSayetharayeu <halobombinas@gmail.com>
2022-06-09 21:13:59 +0200

    -Problem z firebase

---
+++ todoapp-project/projects/tasks/src/lib/adapters/secondary/infrastructure/firebase-tasks.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GetsAllTaskDtoPort } from '../../../application/ports/secondary/gets-all-task.dto-port';
+import { TaskDTO } from '../../../application/ports/secondary/task.dto';
+import { filterByCriterion } from '@lowgular/shared';
+import { AddsTaskDtoPort } from '../../../application/ports/secondary/adds-task.dto-port';
+import { RemovesTaskDtoPort } from '../../../application/ports/secondary/removes-task.dto-port';
+
+@Injectable()
+export class FirebaseTasksService implements GetsAllTaskDtoPort, AddsTaskDtoPort, RemovesTaskDtoPort {
+  constructor(private _client: AngularFirestore) {
+  }
+
+  getAll(criterion: Partial<TaskDTO>): Observable<TaskDTO[]> {
+    return this._client.collection<TaskDTO>('tasks').valueChanges(({idField: 'id'})).pipe(map((data: TaskDTO[]) => filterByCriterion(data, criterion)));
+  }
+
+  add(task: Partial<TaskDTO>): void {
+    this._client.collection('tasks').add(task);
+  }
+
+  remove(id: string): void {
+    this._client.doc('tasks/'+id).delete();
+  }
+}
